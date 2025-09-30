d76eb4e085508c7d10a9429eb611f7edc5d0337d
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2025-09-30 15:04:59 -0700

    Remove PostInfo var

--- components/EntryHeader/EntryHeader.js
+++ components/EntryHeader/EntryHeader.js
@@ -1,7 +1,7 @@
 // EntryHeader.jsx
 import className from 'classnames/bind';
 import { useRouter } from 'next/router';
-import { FeaturedImage, Heading, PostInfo } from 'components';
+import { FeaturedImage, Heading } from 'components';
 
 import styles from './EntryHeader.module.scss';
 
