a3777f410d2058c55181fd2c77bc9888e5f15e8d
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-03-12 20:46:28 -0400

    Add referrer policy header

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ module.exports = withFaust({
             value: 'SAMEORIGIN',
           },
           {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
             key: 'Content-Security-Policy',
             value: "frame-ancestors 'self'",
           },
