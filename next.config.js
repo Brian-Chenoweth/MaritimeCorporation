8e36adf11760dd3bf2f1ab2132bdf39bae14d0f2
Brian Chenoweth <brian.michael.chenoweth@gmail.com>
2026-03-15 19:43:32 -0400

    Add X-Content-Type-Options nosniff header

--- next.config.js
+++ next.config.js
@@ -33,6 +33,10 @@ module.exports = withFaust({
             value: 'SAMEORIGIN',
           },
           {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
             key: 'Referrer-Policy',
             value: 'strict-origin-when-cross-origin',
           },
